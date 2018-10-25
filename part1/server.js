973416239c13661cf3bdd7320bcb02425566b820
Oscar Sandoval <apollo.sandoval@gmail.com>
2018-10-25 11:08:33 -0700

    completed challenge 1-4

--- part1/server.js
+++ part1/server.js
@@ -2,9 +2,32 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 8000;
 
-app.get('/yourroute', function(req, res) {
-  res.send("stuff");
+app.get('/', function(req, res) {
+  res.sendFile('./index.html', {root: __dirname}, (err) => {
+    if(err) throw err;
+    else {
+      console.log('sent');
+    }
+  })
 });
+
+app.get('/hello', function(req, res) {
+  res.send("Hello!");
+});
+
+app.get('/verify/:age', function(req, res) {
+  let age = req.params.age;
+
+  res.sendStatus(age > 13 ? 200 : 403);
+})
+
+app.post('/create/:name', function(req, res) {
+  let obj = {
+    id: 1,
+    name: req.params.name
+  }
+  res.json(obj);
+})
 
 app.use(function(req, res) {
   res.sendStatus(404);
